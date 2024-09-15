308f9a459b03c0880c5eea751c36ba03f825a0e8
陈思 <chensi@chensideMBP.lan>
2024-09-15 19:12:14 -0400

    3try

--- test_main.py
+++ test_main.py
@@ -3,30 +3,31 @@ Test goes here
 
 """
 import pandas as pd
-from main import (load_dataset, 
-                  general_describe, 
-                  generate_vis, 
-                  generate_dist, 
-                  visualize_boxplot
+from main import (
+    load_dataset, 
+    general_describe, 
+    generate_vis, 
+    generate_dist, 
+    visualize_boxplot
 )
 
 
 dataset = "/Users/chensi/Desktop/MIDS/Fall 2024/IDS 706/HW2/StudentPerformanceFactors.csv"
 df = load_dataset(dataset)
 def test_load_dataset():
-    df_test = load_dataset(dataset)  # Ensure this function uses the correct path
-    assert isinstance(df_test, pd.DataFrame), "The loaded dataset should be a DataFrame."
+    df_test = load_dataset(dataset)  
+    assert isinstance(df_test, pd.DataFrame)
 
 # Test the general_describe function for 'Hours_Studied'
 def test_general_describe():
     desc = general_describe(df, 'Hours_Studied')
-    assert round(desc['mean'], 6) == 19.975329, "Mean of 'Hours_Studied' should be 19.975329."
-    assert desc['50%'] == 20.0, "Median (50%) of 'Hours_Studied' should be 20."
-    assert desc['max'] == 44.0, "Max of 'Hours_Studied' should be 44."
-    assert desc['min'] == 1.0, "Min of 'Hours_Studied' should be 1."
-    assert round(desc['std'], 6) == 5.990594, "Standard deviation of 'Hours_Studied' should be 5.990594."
-    assert desc['25%'] == 16.0, "25th percentile of 'Hours_Studied' should be 16."
-    assert desc['75%'] == 24.0, "75th percentile of 'Hours_Studied' should be 24."
+    assert round(desc['mean'], 6) == 19.975329
+    assert desc['50%'] == 20.0
+    assert desc['max'] == 44.0 
+    assert desc['min'] == 1.0
+    assert round(desc['std'], 6) == 5.990594 
+    assert desc['25%'] == 16.0
+    assert desc['75%'] == 24.0 
 
 # Test the generate_vis function for 'Hours_Studied' vs 'Exam_Score'
 def test_generate_vis():
