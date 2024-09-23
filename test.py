982d539c5015c81c33b96eea7a8fa6442865fb5a
陈思 <chensi@chensideMBP.lan>
2024-09-22 22:29:28 -0400

    final

--- test.py
+++ test.py
@@ -4,15 +4,17 @@ from main import load_dataset_pl, general_describe_pl
 
 dataset_path = "StudentPerformanceFactors.csv"
 
-
+# Add a loop to increase the profiling duration
 with Profiler(interval=0.1) as profiler_pandas:
     df_pandas = load_dataset(dataset_path)
-    stats_pandas = general_describe(df_pandas, 'Hours_Studied')
-    print("Pandas Stats:", stats_pandas)
+    stats_pandas = general_describe(df_pandas, "Hours_Studied")
+    print(stats_pandas)
+
 profiler_pandas.print()
 
 with Profiler(interval=0.1) as profiler_polars:
     df_polars = load_dataset_pl(dataset_path)
-    stats_polars = general_describe_pl(df_polars, 'Hours_Studied')
-    print("Polars Stats:", stats_polars)
+    stats_polars = general_describe_pl(df_polars, "Hours_Studied")
+    print(stats_polars)
+
 profiler_polars.print()
